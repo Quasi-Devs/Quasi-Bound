500e9021563beb24143376b8aab666bbfad8998b
Billy <nbilly319@gmail.com>
2020-12-22 08:36:11 -0600

    (update) got some of the 2DEnv to display

--- client/src/components/GameEnv/2DEnv/2DEnv.jsx
+++ client/src/components/GameEnv/2DEnv/2DEnv.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './2denv.css';
 
 const TwoDEnv = () => {
   const text = '2D environment';
+  const resource = [true, true, true, true, true, true, true, true, false, false, false, false];
   return (
-    <div>{text}</div>
+    <div>
+      {text}
+      <div className="Resourceholder">
+        {resource.map((val, i) => <div key={`${String(i)}`} style={{ backgroundColor: val ? 'blue' : null }} className="ResourcePoints">{}</div>)}
+      </div>
+      <div className="cards">
+        placeholder for cards
+      </div>
+      <button type="submit">Surrender</button>
+      <button type="submit">End Turn</button>
+    </div>
   );
 };
 
