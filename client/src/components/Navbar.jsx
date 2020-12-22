e5933c2e8df18ac1e77742aafbc22cf5400028f9
benyoss <benyoss4@gmail.com>
2020-12-22 12:12:34 -0600

    (create) basic styling layout for navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
-import { AppBar, Toolbar } from '@material-ui/core';
+import { AppBar, Toolbar, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
-const Navbar = () => {
+const useStyles = makeStyles(() => ({
+  profileButton: {
+    marginRight: '5%',
+    position: 'absolute',
+    right: 0,
+    display: 'flex',
+    flexFlow: 'row wrap',
+    overflowWrap: 'inherit',
+  },
+  button: {
+    marginLeft: '5%',
+    display: 'flex',
+    flexFlow: 'row wrap',
+    overflowWrap: 'inherit',
+  },
+  container: {
+    display: 'flex',
+    flexFlow: 'row wrap',
+  },
+}));
+
+const Navbar = ({ user }) => {
+  const classes = useStyles();
   const text = 'navbar';
   return (
-    <AppBar position="static">
-      <Toolbar>
+    <AppBar position="sticky">
+      <Toolbar className={classes.container}>
         <div>{text}</div>
-        <button type="button">Google Translate</button>
-        <button type="button">Play</button>
+        <Button variant="contained" className={classes.button} color="primary" type="button">Google Translate</Button>
+        <Button variant="contained" className={classes.button} color="primary" type="button">Play</Button>
+        {!user.session ? (<Button variant="contained" color="primary" type="button" className={classes.profileButton}>Login</Button>)
+          : (<Button variant="contained" color="primary" type="button" className={classes.profileButton} onClick={() => { window.location.href = '/profile'; }}>Profile</Button>)}
       </Toolbar>
     </AppBar>
   );
+};
+
+Navbar.propTypes = {
+  user: PropTypes.element.isRequired,
 };
 
 export default Navbar;
