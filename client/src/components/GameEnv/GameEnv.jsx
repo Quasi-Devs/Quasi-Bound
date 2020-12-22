978c51a3de2f4cb430b84f9692d6fdab37463221
Billy <nbilly319@gmail.com>
2020-12-21 19:24:47 -0600

    (add) the basic playhub page

--- client/src/components/GameEnv/GameEnv.jsx
+++ client/src/components/GameEnv/GameEnv.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import ThreeDEnv from './3DEnv/3DEnv';
 
 const GameEnv = () => {
   const text = 'game environment';
   return (
-    <div>{text}</div>
+    <div>
+      <h1>{text}</h1>
+      <ThreeDEnv />
+    </div>
   );
 };
 
