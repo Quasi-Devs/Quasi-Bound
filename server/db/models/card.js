a5f29a7f1bd4d8d3784d1b9865d6003d77032597
benyoss <benyoss4@gmail.com>
2020-12-29 00:00:09 -0600

    (create) database methods for Card

--- server/db/models/card.js
+++ server/db/models/card.js
@@ -1,14 +1,23 @@
 const db = require('../index');
 
-const createCard = async (rp, thumbnail, description, attack, health, armor, isCharacter, size) => {
-  const result = await db.query(`INSERT INTO "card" (point_resource, thumbnail, description, point_attack, point_health, point_armor, is_character, size) VALUES (
-      '${rp}', '${thumbnail}', '${description}', '${attack}', '${health}', '${armor}', '${rp}', ${isCharacter}, '${size}'
+const createCard = async (
+  {
+    title, rp, thumbnail, description, attack, health, armor, isCharacter, size,
+  },
+) => {
+  const result = await db.query(`INSERT INTO "card" (point_resource, thumbnail, description, point_attack, point_defence, point_armor, is_character, size, title) VALUES (
+      '${rp}', '${thumbnail}', '${description}', '${attack}', '${health}', '${armor}', ${isCharacter}, '${size}', '${title}'
       );`);
   return result;
 };
 
-const getCards = async (options) => {
-  const cards = await db.query(`SELECT * FROM "card" WHERE ${options}`);
+const getCards = async ({ key, value }) => {
+  let cards;
+  if (key && value) {
+    cards = await db.query(`SELECT * FROM "card" WHERE ${key} = ${value}`);
+  } else {
+    cards = await db.query('SELECT * FROM "card"');
+  }
   return cards;
 };
 
