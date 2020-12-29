909ab8b5e3160e2ebf172d383c35eab6732d6797
BenYoss <benyoss4@gmail.com>
2020-12-29 15:50:21 -0600

    (fix) removal of logs

--- server/routes/dbRouter.js
+++ server/routes/dbRouter.js
@@ -6,7 +6,6 @@ const dbRouter = Router();
 dbRouter.get('/cards', async (req, res) => {
   Card.getCards(req.query)
     .then((cards) => {
-      console.info(cards);
       res.status(200).send(cards);
     })
     .catch((err) => { console.error(err); res.sendStatus(500); });
