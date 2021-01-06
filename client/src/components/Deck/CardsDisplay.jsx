7a60e861ca9d907db49d89d94957039f3aed02db
Andrew Gremillion <agremillion15@gmail.com>
2021-01-05 19:12:54 -0600

    (add) user cards displayed on CardDisplay page

--- client/src/components/Deck/CardsDisplay.jsx
+++ client/src/components/Deck/CardsDisplay.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
+import { Grid } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
-const CardsDisplay = () => {
-  const text = 'cards display';
+import Card from './Card';
+
+const CardsDisplay = ({ user }) => {
+  // const [userId, setUserId] = useState(user ? user.id : null);
+  const [myCards, setMyCards] = useState([]);
+  useLayoutEffect(() => {
+    const fetchData = async () => {
+      if (user) {
+        const { data: cardData } = await axios.get(`/data/userCards/${user.id}`);
+        setMyCards(cardData);
+      }
+    };
+    fetchData();
+  }, [user]);
+
   return (
-    <div>{text}</div>
+    <div>
+      <Grid container direction="row" justify="space-around" alignItems="center" md={5}>
+        {myCards.map((card) => <Card key={card} card={card} />)}
+      </Grid>
+    </div>
   );
+};
+
+CardsDisplay.propTypes = {
+  user: PropTypes.shape().isRequired,
 };
 
 export default CardsDisplay;
